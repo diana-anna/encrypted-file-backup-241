00a271efa9cd7d27cb7595ab95d355cfebd426a8
Claire Boyan <cboyan2@illinois.edu>
2019-01-26 15:36:46 -0600

    not much :(

--- extreme_edge_cases/camelCaser.c
+++ extreme_edge_cases/camelCaser.c
@@ -10,37 +10,67 @@
 #include <string.h>
 
 char **camel_caser(const char *input_str) {
+	// dimension 1 = # sentences
+	// dimension 2 = # chars in sentence
 	return NULL;
 }
 
+char *camel_caser_hf( char *input_str) {
+	if (input_str == NULL) {
+		return NULL;
+	}
+
+	input_str = capitalizer(input_str);
+	input_str = unspacer(input_str);
+	return input_str;
+
+	// int start_index = 0;
+	// int punct_index = 0;
+	// while (*input_str) {
+	// 	char* sentence;
+	// 	while (*input_str && (is_letter(*input_str) || *input_str == 32)) { // while it is a letter or space
+	// 	punct_index++;
+	// 	input_str++;
+	// 	}
+	// 	// from start_index to punct_index is a sentence
+	// 	memmove(&sentence, &input_str - punct_index, punct_index);
+
+	// }
+
+}
+
 // helper function to capitalize all words but first
-char *capitalizer(const char *input_str) {
-    char *capitalized = malloc(strlen(input_str) + 1);
-    strcpy(capitalized, input_str);
+char* capitalizer( char* input_str) {
+	char capitalized[strlen(input_str) + 1]; // editable copy
+	strcpy(capitalized, input_str);
 
-    size_t j;
-    for (j = 0; j < strlen(capitalized); j++) {
-	capitalized[j] = tolower(capitalized[j]);
-    }
+	size_t j;
+	for (j = 0; j < strlen(capitalized); j++) {
+		capitalized[j] = tolower(capitalized[j]);
+	}
 
-    int cap = 0;
-    int have_we_hit_a_letter_yet = 0; // account for extra spaces
-    size_t i;
-    for (i = 0; i < strlen(input_str); i++) {
-          if (cap && is_letter(capitalized[i])) {
-            capitalized[i] -= 32;
-            cap = 0;
-          } else if (capitalized[i] == 32) {
-            cap = 1;
-          }
-	  if ((!have_we_hit_a_letter_yet) && is_letter(capitalized[i])){
-	    have_we_hit_a_letter_yet = 1;
-	    cap = 0;
-          }
-    }
-    char* result = malloc(strlen(input_str) + 1);
-    strcpy(result, capitalized);
-    return result;
+	int cap = 0;
+	int have_we_hit_a_letter_yet = 0;
+
+	size_t i;
+	for (i = 0; i < strlen(input_str); i++) {
+
+
+		if (cap && is_letter(capitalized[i]) && have_we_hit_a_letter_yet) {
+			capitalized[i] -= 32;
+			cap = 0;
+
+		} else if (capitalized[i] == 32) {
+			cap = 1;
+		}
+		if ((!have_we_hit_a_letter_yet) && is_letter(capitalized[i])){
+			have_we_hit_a_letter_yet = 1;
+			cap = 0;
+		}
+	}
+	input_str = strdup(capitalized);
+
+	return input_str;
 }
 
 char *unspacer(char* str) {
@@ -56,10 +86,12 @@ char *unspacer(char* str) {
 			i--;
 		}
 	}
-	char* result = malloc(strlen(spaced) + 1);
-	strcpy(result, spaced);
-	
-	return result;
+	// char* result = malloc(strlen(spaced) + 1);
+	// strcpy(result, spaced);
+
+	str = strdup(spaced);
+
+	return str;
 }
 
 // helper function to check if a char is a letter
--- extreme_edge_cases/camelCaser.h
+++ extreme_edge_cases/camelCaser.h
@@ -5,7 +5,8 @@
  
 #pragma once
 char **camel_caser(const char *input_str);
+char *camel_caser_hf( char *input_str);
 void destroy(char **result);
-char* capitalizer(const char *input_str);
+char* capitalizer(char *input_str);
 char *unspacer(char* str);
 int is_letter(char str);
