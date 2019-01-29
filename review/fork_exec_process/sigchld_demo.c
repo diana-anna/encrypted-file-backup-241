f0440effca96845f4404f8faad643b89c316fb37
Aneesh Durg <durg2@illinois.edu>
2019-01-29 15:03:13 -0600

    more macro magic for interested parties

--- review/fork_exec_process/sigchld_demo.c
+++ review/fork_exec_process/sigchld_demo.c
@@ -5,8 +5,10 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#define str(x...) xstr(x)
+#define xstr(x...) #x
 #define LOG_AND_RUN(PROGRAM...)                                                \
-  printf("line %d: "#PROGRAM"=%d\n", __LINE__, PROGRAM)
+  printf("line "str(__LINE__)": "str(PROGRAM)"=%d\n", PROGRAM)
 
 void handler(int sig) {
   (void)sig;
@@ -16,10 +18,13 @@ void handler(int sig) {
 
 int main() {
   signal(SIGCHLD, handler);
-  if (!fork()) {
+  pid_t child;
+  if (!(child = fork())) {
     sleep(5);
     puts("Child is exiting");
     exit(0);
+  } else {
+    printf("Child pid is %d\n", child);
   }
 
 
@@ -28,7 +33,7 @@ int main() {
 
   alarm(10);
   while (1) {
-    puts("Parent is alive");
+    puts("  Parent is alive");
     sleep(1);
   }
 }
