ddcfa1482493441574ddeb8e7364284a30eb72cc
Claire Boyan <cboyan2@illinois.edu>
2019-02-04 19:14:56 -0600

    making progressgit add -u

--- vector/sstring.c
+++ vector/sstring.c
@@ -34,11 +34,12 @@ sstring *cstr_to_sstring(const char *input) {
 char *sstring_to_cstr(sstring *input) {
     // your code goes here
     size_t sz = vector_size(input->v);
-    char* cstr = (char*) malloc(sz * sizeof(char));
+    char* cstr = (char*) malloc((sz * sizeof(char)) + 1);
     size_t i;
     for (i = 0; i < sz; i++) {
       cstr[i] = *(char*) vector_get(input->v, i);
     }
+    cstr[i] = 0;
     return cstr;
 }
 
@@ -54,7 +55,32 @@ int sstring_append(sstring *this, sstring *addition) {
 
 vector *sstring_split(sstring *this, char delimiter) {
     // your code goes here
-    return NULL;
+    char* this_cstr = sstring_to_cstr(this);
+    size_t begin = 0;
+    vector* split = vector_create(char_copy_constructor, char_destructor, char_default_constructor);
+    size_t i;
+    int count = 0;
+    for (i = 0; i < strlen(this_cstr); i++) {
+      count++;
+      if (this_cstr[i] == delimiter) {
+        char* word = malloc((sizeof(char) * count) );
+        memmove(word, &this_cstr[i - (count - 1)], count);
+        word[count - 1] = 0;
+        vector_push_back(split, word);
+        begin = i;
+        count = 0;
+        free(word);
+      }
+    }
+    // final word:
+    // plus 1 because count increments at beginning of loop and we exited
+    char* word = malloc((sizeof(char) * count) + 1);
+    memmove(word, &this_cstr[i - (count - 1)], count);
+    word[count] = 0;
+    vector_push_back(split, word);
+    free(word); 
+
+    return split;
 }
 
 int sstring_substitute(sstring *this, size_t offset, char *target,
--- vector/sstring_test.c
+++ vector/sstring_test.c
@@ -4,8 +4,20 @@
  */
  
 #include "sstring.h"
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     // TODO create some tests
+    char* test = "this is a sentence";
+    sstring* test_ss = cstr_to_sstring(test);
+    char* test2 = sstring_to_cstr(test_ss);
+    assert(!strcmp(test, test2));
+    
+    sstring* test_ss2 = cstr_to_sstring("hi");
+    sstring* test_ss3 = cstr_to_sstring("hola");
+    assert(sstring_append(test_ss2, test_ss3) == 6);
+
     return 0;
 }
