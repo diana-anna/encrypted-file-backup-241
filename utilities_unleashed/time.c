6cb1a9e01a1da81c8c3dd11ac79b26edfa208c86
Claire Boyan <cboyan2@illinois.edu>
2019-02-06 14:44:52 -0600

    it worked for sleep 2

--- utilities_unleashed/time.c
+++ utilities_unleashed/time.c
@@ -5,6 +5,7 @@
  
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <sys/wait.h>
 #include <sys/types.h>
@@ -16,22 +17,35 @@ int main(int argc, char *argv[]) {
 	print_time_usage();
 	exit(0);
     }
-
+    
+    char** child_argv = malloc(sizeof(char*) * argc + 1);
+    size_t i;
+    for (i = 1; i < (size_t) argc; i++) {
+    	child_argv[i - 1] = argv[i];
+    }
+    child_argv[i - 1] = 0;
+    
     pid_t child_id = fork();
     printf("fork executed.\n");
+    
     if (child_id == -1) {
 	print_fork_failed();
 	exit(1);
     }
+
     struct timespec begin, end;
-    if (child_id) {
+    if (child_id) { // parent
+	printf("parent reached\n");
 	int status;
 	clock_gettime(CLOCK_MONOTONIC, &begin);
 	waitpid(child_id, &status, 0);
 	clock_gettime(CLOCK_MONOTONIC, &end);
-        // do math stuff
-     } else {
-	if (execvp(argv[1], &argv[1]) < 0) {
+	time_t seconds = end.tv_sec - begin.tv_sec;
+	// for testing - DELETE LATER
+	printf("seconds = %f\n", (double)seconds);
+    } else { // child
+	printf("child reached\n");
+	if (execvp(child_argv[0], child_argv) < 0) {
 	    print_exec_failed();
             exit(1);
 	}
