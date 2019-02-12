9c3f5eb7b84680e826dfe609c97aa75b747bece3
Jing Rong Lim <jrlim3@illinois.edu>
2019-02-12 11:24:14 -0600

    second exercise

---
+++ review/malloc_and_memory/sbrk_alloc/sbrk_exercises/sbrk_two.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+  unsigned int * sizePtrOne = sbrk(/* SOME VALUE HERE */);
+  *sizePtrOne = /* SOME VALUE HERE */;
+  int* intPtr = (sizePtrOne + /* SOME VALUE HERE */);
+  *intPtr = 241;
+
+  unsigned int * sizePtrTwo = sbrk(/* SOME VALUE HERE */);
+  *sizePtrTwo = /* SOME VALUE HERE */;
+  int** addressPtr = (sizePtrTwo + /* SOME VALUE HERE */);
+  *addressPtr = intPtr;
+
+  unsigned int * sizePtrThree = sbrk(/* SOME VALUE HERE */);
+  *sizePtrThree = /* SOME VALUE HERE */;
+  char* string = (sizePtrThree + /* SOME VALUE HERE */);
+  strcpy(string, "Hello world");
+
+  printf("%p", sbrk(/* SOME VALUE HERE */));
+
+  return 0;
+}
