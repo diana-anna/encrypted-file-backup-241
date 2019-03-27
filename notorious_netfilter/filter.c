dc16399068b852fab1607d2f2ee362eec268d575
Aneesh Durg <durg2@illinois.edu>
2019-03-27 15:32:17 -0500

    Deploying notorious_netfilter

---
+++ notorious_netfilter/filter.c
@@ -0,0 +1,19 @@
+/**
+ * Notorious Netfilter
+ * CS 241 - Spring 2019
+ */
+#include <linux/ip.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/netdevice.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/proc_fs.h>
+#include <linux/skbuff.h>
+#include <linux/slab.h>
+#include <linux/udp.h>
+
+#include "filter.h"
+
+// Good luck!
